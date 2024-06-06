2de0f1a2a567d6ff5e9be5cce1822db579f72a57
jcody49 <123919470+jcody49@users.noreply.github.com>
2024-06-05 18:41:03 -0600

    Fixes almost all resizing issues

--- JS/scripts.js
+++ JS/scripts.js
@@ -10,13 +10,17 @@ $(document).ready(function(){
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
+                    centerMode: true,
+                    centerPadding: '0'
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    centerMode: true,
+                    centerPadding: '0'
                 }
             }
         ]
@@ -24,3 +28,38 @@ $(document).ready(function(){
     
     console.log("Slider initialized:", slider);
 });
+$(document).ready(function(){
+    console.log("Initializing slick slider for case studies...");
+    $('.case-studies').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-prev">&#10094;</button>',
+        nextArrow: '<button class="slick-next">&#10095;</button>',
+        dots: true, // Add pagination dots
+    });
+});
+$(document).ready(function(){
+    console.log("Initializing slick slider for case studies...");
+    var slider = $('.case-studies').slick({
+        slidesToShow: 3, // Show 3 cards at a time on larger screens
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-prev">&#10094;</button>',
+        nextArrow: '<button class="slick-next">&#10095;</button>',
+        dots: true, // Add pagination dots
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1, // Show only 1 card at a time on mobile screens
+                    centerMode: true,
+                    centerPadding: '0'
+                }
+            }
+        ]
+    });
+    
+    console.log("Slider initialized for case studies:", slider);
+});
+
+
+
